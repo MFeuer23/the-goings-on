ca471e759a2fb8514f2d5382a3084b55c6810d66
Mallory <mallory.feuer@gmail.com>
2019-03-09 18:59:49 -0500

    fed state to fetchEvents action as a param

--- client/src/EventsForm.js
+++ client/src/EventsForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchEvents } from '../actions/EventActions'
+import { fetchEvents } from './actions/EventActions'
 
 class EventsForm extends Component {
   constructor() {
--- client/src/actions/EventActions.js
+++ client/src/actions/EventActions.js
@@ -1,7 +1,8 @@
-export function fetchEvents() {
+export function fetchEvents(state) {
+  console.log(state)
   return (dispatch) => {
-    dispatch({ type: 'LOADING_EVENTS' });
-    return fetch('http://localhost:3001/events')
+    dispatch({ type: 'LOADING_EVENTS', state});
+    return fetch(`http://localhost:3001/events/${state}`)
       .then(response => {return response.json() })
       .then(responseJSON => {return responseJSON})
       .then(payload => dispatch({ type: 'FETCH_EVENTS', payload }));
