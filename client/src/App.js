69815788e2245ee0dcacb643851719c9423d4b01
Mallory <mallory.feuer@gmail.com>
2019-03-09 18:28:54 -0500

    add EventsForm to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import './App.css';
+import EventsForm from './EventsForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <EventsForm />
 
 
-          
         </header>
       </div>
     );
