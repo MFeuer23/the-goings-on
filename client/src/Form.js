20892c32f3048352ae87c0b8a2aac4a39d6a6174
Mallory <mallory.feuer@gmail.com>
2019-03-09 18:06:30 -0500

    add form component

---
+++ client/src/Form.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class Form extends Component {
+  constructor() {
+    super();
+    this.state = {
+      category: '',
+      ongoing: ''
+    }
+  }
+
+  render(){
+    return (
+      
+    )
+  }
+
+}
+
+export default Form;
