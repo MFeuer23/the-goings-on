6343bcefb463d9abe2d46c5f075cabad37da4e1d
Mallory <mallory.feuer@gmail.com>
2019-03-09 20:21:24 -0500

    change fetch request to use proxy

--- client/src/actions/EventActions.js
+++ client/src/actions/EventActions.js
@@ -2,7 +2,7 @@ export function fetchEvents(state) {
   console.log(state)
   return (dispatch) => {
     dispatch({ type: 'LOADING_EVENTS', state});
-    return fetch(`http://localhost:3001/events/${state}`)
+    return fetch(`events/${state}`, {accept: 'application/json',})
       .then(response => {return response.json() })
       .then(responseJSON => {return responseJSON})
       .then(payload => dispatch({ type: 'FETCH_EVENTS', payload }));
