87ced4020bf567b534a1f2441fa11d0451cca580
Mallory <mallory.feuer@gmail.com>
2019-03-09 20:48:37 -0500

    change the structure of params

--- client/src/actions/EventActions.js
+++ client/src/actions/EventActions.js
@@ -1,8 +1,8 @@
 export function fetchEvents(state) {
-  console.log(state)
+  console.log(state.category)
   return (dispatch) => {
     dispatch({ type: 'LOADING_EVENTS', state});
-    return fetch(`events/${state}`, {accept: 'application/json',})
+    return fetch(`events/${state.category}/${state.status}`, {accept: 'application/json',})
       .then(response => {return response.json() })
       .then(responseJSON => {return responseJSON})
       .then(payload => dispatch({ type: 'FETCH_EVENTS', payload }));
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/events/:params', to: 'events#events'
+  get '/events/:category/:status', to: 'events#events'
 end
