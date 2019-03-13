c9bfc750519c8bdb7f59b0fcf6deaa35c5f678e4
Mallory <mallory.feuer@gmail.com>
2019-03-12 22:37:01 -0400

    add link to bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,32 @@ import './App.css';
 import EventsForm from './EventsForm'
 import EventsContainer from './containers/EventsContainer'
 
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <header className="App-body">
         <EventsForm />
+        <br/>
         <EventsContainer events={this.props.eventInfo}/>
 
 
         </header>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
+          integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+          crossOrigin="anonymous"
+        />
       </div>
+
     );
   }
 }
 
 const mapStateToProps = state => {
-  debugger;
+
   return {
     eventInfo: state.events.data.events
   }
