535683fd548961ebb151e470ba978b43339c2521
Mallory <mallory.feuer@gmail.com>
2019-03-12 22:40:26 -0400

    add Card and Responsive Embed from Bootstrap

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+
 
 const Event = props => {
   console.log(props)
   return(
-    <li>{props.info.title}</li>
+    <Card bg="secondary">
+      <Card.Body>
+        <Card.Title>{props.info.title}</Card.Title>
+        <Card.Text>{props.info.description}</Card.Text>
+        <ResponsiveEmbed>
+          <embed type="image/svg+xml" src={props.info.sources[0].url} />
+        </ResponsiveEmbed>
+      </Card.Body>
+
+    </Card>
   )
 
 }
