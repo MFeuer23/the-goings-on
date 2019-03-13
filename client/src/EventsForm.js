78730efcce6bd2ff9b2a753b73546f8ca55c7dbd
Mallory <mallory.feuer@gmail.com>
2019-03-12 22:36:29 -0400

    add button styling from bootstrap

--- client/src/EventsForm.js
+++ client/src/EventsForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchEvents } from './actions/EventActions'
+import Button from 'react-bootstrap/Button';
 
 class EventsForm extends Component {
   constructor() {
@@ -24,33 +25,36 @@ class EventsForm extends Component {
 
   render(){
     return (
-      <form onSubmit={event => this.handleSubmit(event)}>
-        Category:
-        <select name="category" onChange={this.handleChange}>
-          <option value="all">All</option>
-          <option value="6">Drought</option>
-          <option value="7">Dust and Haze</option>
-          <option value="16">Earthquakes</option>
-          <option value="9">Floods</option>
-          <option value="14">Landslides</option>
-          <option value="19">Manmade</option>
-          <option value="15">Sea and Lake Ice</option>
-          <option value="10">Severe Storms</option>
-          <option value="17">Snow</option>
-          <option value="18">Temperature Extremes</option>
-          <option value="12">Volcanoes</option>
-          <option value="13">Water Color</option>
-          <option value="8">Wildfires</option>
-        </select>
-        <br />
-        Status:
-        <select name="status" onChange={this.handleChange}>
-          <option value="open">Ongoing</option>
-          <option value="closed">Past</option>
-        </select>
-        <br />
-        <input type="submit"/>
-      </form>
+      <div>
+        <form onSubmit={event => this.handleSubmit(event)}>
+          Category:
+          <select name="category" onChange={this.handleChange}>
+            <option value="all">All</option>
+            <option value="6">Drought</option>
+            <option value="7">Dust and Haze</option>
+            <option value="16">Earthquakes</option>
+            <option value="9">Floods</option>
+            <option value="14">Landslides</option>
+            <option value="19">Manmade</option>
+            <option value="15">Sea and Lake Ice</option>
+            <option value="10">Severe Storms</option>
+            <option value="17">Snow</option>
+            <option value="18">Temperature Extremes</option>
+            <option value="12">Volcanoes</option>
+            <option value="13">Water Color</option>
+            <option value="8">Wildfires</option>
+          </select>
+          <br />
+          Status:
+          <select name="status" onChange={this.handleChange}>
+            <option value="open">Ongoing</option>
+            <option value="closed">Past</option>
+          </select>
+          <br />
+          <Button variant="outline-danger" type="submit">Submit</Button>
+        </form>
+
+      </div>
     )
   }
 
