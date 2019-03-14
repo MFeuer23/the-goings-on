8fd323c971a263fdff06e2c4be2a2e270eb3c4fb
Mallory <mallory.feuer@gmail.com>
2019-03-13 21:17:41 -0400

    created star favorite button

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+import Button from 'react-bootstrap/Button'
+import '../App.css'
 
 
 const Event = props => {
   console.log(props)
   return(
-    <Card bg="secondary">
-      <Card.Body>
-        <Card.Title>{props.info.title}</Card.Title>
-        <Card.Text>{props.info.description}</Card.Text>
-        <ResponsiveEmbed>
-          <embed type="image/svg+xml" src={props.info.sources[0].url} />
-        </ResponsiveEmbed>
-      </Card.Body>
+    <div>
+      <Card bg="secondary">
 
-    </Card>
+        <Card.Body>
+          <Card.Title>
+            <input id="check" type="checkbox" class="fa fa-star"></input>
+            <br/>
+            {props.info.title}
+          </Card.Title>
+          <Card.Text>{props.info.description}</Card.Text>
+          <ResponsiveEmbed>
+            <embed type="image/svg+xml" src={props.info.sources[0].url} />
+          </ResponsiveEmbed>
+        </Card.Body>
+
+      </Card>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    </div>
   )
 
 }
