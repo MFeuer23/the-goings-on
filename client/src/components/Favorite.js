2b34d97631248eea9ae9a5e7648859f93543a726
Mallory <mallory.feuer@gmail.com>
2019-03-16 13:50:14 -0400

    create Favorite stateless component to display favorites

---
+++ client/src/components/Favorite.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card'
+import ResponsiveEmbed from 'react-bootstrap/ResponsiveEmbed'
+import Button from 'react-bootstrap/Button'
+
+
+
+const Event = props => {
+  console.log(props)
+
+  return(
+    <div>
+      <Card bg="secondary" style={{ width: '14rem' }}>
+
+        <Card.Body>
+          <Card.Title>
+            <input id="check" type="checkbox" className="fa fa-star" checked></input>
+            <br/>
+            {props.info.title}
+          </Card.Title>
+          <br/>
+          <Card.Subtitle>{props.info.description}</Card.Subtitle>
+          <br/>
+          <Card.Link target="_blank" style={{ color: '#8b0000' }} href={props.info.source_url}>Source Link</Card.Link>
+          <br/>
+          <Card.Footer>{props.info.category}</Card.Footer>
+        </Card.Body>
+
+      </Card>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
+    </div>
+  )
+
+}
+
+export default Event;
