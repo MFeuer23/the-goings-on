1bd82491a24641bfd1ba0c2f1f7831eedd89d664
Mallory <mallory.feuer@gmail.com>
2019-03-16 13:43:07 -0400

    add error catching for creating a favorite that already exists, add fetchFavorites()

--- client/src/actions/FavoriteActions.js
+++ client/src/actions/FavoriteActions.js
@@ -5,9 +5,22 @@ export function createFavorite(title, description, category, source_url) {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ title, description, category, source_url })
     })
-      .then(response => response.json())
+      .then(response => {return response.json() })
+      .then(responseJSON => {return responseJSON})
       .then(payload => {
         dispatch({ type: 'CREATE_FAVORITE', payload });
-      });
+      }).catch(function(error) {
+    console.log('request failed, already in favorites', error)
+  })
+  };
+}
+
+export function fetchFavorites() {
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_FAVORITES' });
+    return fetch('favorites', {accept: 'application/json',})
+      .then(response => {return response.json() })
+      .then(responseJSON => {return responseJSON})
+      .then(payload => dispatch({ type: 'FETCH_FAVORITES', payload }));
   };
 }
