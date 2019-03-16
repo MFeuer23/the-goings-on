aa202501424f705f0cfe172d181732bf152a0b70
Mallory <mallory.feuer@gmail.com>
2019-03-15 22:34:32 -0400

    changed limit and sources of API calls

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,9 +2,9 @@ class EventsController < ApplicationController
   def events
 
     if params[:category] == "all"
-      url = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?source=EO&limit=3&status=#{params[:status]}"
+      url = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/events?limit=4&status=#{params[:status]}"
     else
-      url = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/#{params[:category]}?source=EO&limit=3&status=#{params[:status]}"
+      url = "https://eonet.sci.gsfc.nasa.gov/api/v2.1/categories/#{params[:category]}?limit=4&status=#{params[:status]}"
     end
 
     begin
