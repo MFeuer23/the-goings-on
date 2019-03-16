282dde6d0e8ef6b1c535a276db75110a96826ecd
Mallory <mallory.feuer@gmail.com>
2019-03-16 10:34:58 -0400

    add favoritesReducer to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import eventsReducer from "./events_reducer"
+import favoritesReducer from './favorites_reducer'
 
 const rootReducer = combineReducers({
-  events: eventsReducer
+  events: eventsReducer,
+  favorites: favoritesReducer
 })
 
 export default rootReducer;
