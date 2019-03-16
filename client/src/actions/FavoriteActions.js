ed1dd94c740b79fb36f86c47c95d12a1d837a1f3
Mallory <mallory.feuer@gmail.com>
2019-03-16 12:05:22 -0400

    add createFavorite function with post request to API

--- client/src/actions/FavoriteActions.js
+++ client/src/actions/FavoriteActions.js
@@ -1,9 +1,9 @@
-export function createFavorite(title, description, catagory, source_url) {
+export function createFavorite(title, description, category, source_url) {
   return dispatch => {
     return fetch('favorites', {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ title, description, catagory, source_url })
+      body: JSON.stringify({ title, description, category, source_url })
     })
       .then(response => response.json())
       .then(payload => {
