ad50527e7387eec296c52daf8fde6496ff8b49a6
Mallory <mallory.feuer@gmail.com>
2019-03-16 13:41:37 -0400

    add index action

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
 class FavoritesController < ApplicationController
 
 
+  def index
+    render :json => Favorite.all
+  end
 
-    def create
-      @favorite = Favorite.find_or_create_by(
-        title: params["title"],
-        description: params["description"],
-        source_url: params["source_url"],
-        category: params["category"]
-      )
-      binding.pry
-    end
+  def create
+    @favorite = Favorite.find_or_create_by(
+      title: params["title"],
+      description: params["description"],
+      source_url: params["source_url"],
+      category: params["category"]
+    )
+  end
 
 
 end
