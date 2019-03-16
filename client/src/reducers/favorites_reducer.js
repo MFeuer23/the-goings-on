5cc75d8a3aebaa3218244f537b4d605f51be861d
Mallory <mallory.feuer@gmail.com>
2019-03-16 13:44:04 -0400

    add cases for Loading and Fetching Favorites

--- client/src/reducers/favorites_reducer.js
+++ client/src/reducers/favorites_reducer.js
@@ -5,8 +5,17 @@ function eventsReducer(state = {
 }, action) {
   switch (action.type) {
     case "CREATE_FAVORITE":
-    console.log(action)
+      console.log(action)
       return {...state, favoritesData: [...state.favoritesData, action.payload]}
+
+    case "LOADING_FAVORITES":
+      console.log(action)
+      return {...state, loading: true, favoritesData:[], }
+
+    case "FETCH_FAVORITES":
+              // debugger
+      console.log(action)
+      return {...state, loading:false, favoritesData: action.payload}
 
     default:
       return state
