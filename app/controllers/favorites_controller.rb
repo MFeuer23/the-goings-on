6b879280c737c0ed217c8a2c6ff05a86d831b683
Mallory <mallory.feuer@gmail.com>
2019-03-16 12:15:07 -0400

    add create action to favorites_controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,16 @@
 class FavoritesController < ApplicationController
 
-  
+
+
+    def create
+      @favorite = Favorite.find_or_create_by(
+        title: params["title"],
+        description: params["description"],
+        source_url: params["source_url"],
+        category: params["category"]
+      )
+      binding.pry
+    end
 
 
 end
