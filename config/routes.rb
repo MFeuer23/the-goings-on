3716760c16fd96640d523b7883786bdeb6a4c259
Mallory <mallory.feuer@gmail.com>
2019-03-17 14:53:36 -0400

    fix typo, delete to destroy route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/events/:category/:status', to: 'events#events'
 
-  resources :favorites, only: [:index, :create, :delete]
+  resources :favorites, only: [:index, :create, :destroy]
 end
