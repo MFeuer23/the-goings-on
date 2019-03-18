65881db4b0e59c508ad94d8fca47a8b895b64bb3
Mallory <mallory.feuer@gmail.com>
2019-03-18 03:19:10 -0400

    remove logo

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,23 +2,19 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 
-
 const NavBar = props => {
-  console.log(props)
 
   return(
     <Navbar bg="danger" variant="light" sticky="top">
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
-        <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/">Home</Nav.Link>
           <Nav.Link href="/about">About</Nav.Link>
           <Nav.Link href="/favorites">Favorites</Nav.Link>
-         </Nav>
+        </Nav>
       </Navbar.Collapse>
-
-     </Navbar>
+    </Navbar>
   )
 
 }
