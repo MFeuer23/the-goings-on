e45b6824987cf1df752ac770f363379ff390669d
Mallory <mallory.feuer@gmail.com>
2019-03-18 03:18:48 -0400

    add Header and route for About

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import './App.css';
 import EventsContainer from './containers/EventsContainer'
 import FavoritesContainer from './containers/FavoritesContainer'
 import NavBar from './components/Navbar'
+import About from './components/About'
+import Header from './components/HeaderImg'
+
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { fetchFavorites } from './actions/FavoriteActions'
 
@@ -16,27 +19,24 @@ class App extends Component {
 
   render() {
     return (
-
-        <Router>
+      <Router>
         <div className="App App-body">
+          <Header />
           <NavBar />
           <br/>
           <Route exact path="/" component={EventsContainer}/>
           <Route exact path="/favorites" component={FavoritesContainer}/>
+          <Route exact path="/about" component={About}/>
           <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css"
             integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
             crossOrigin="anonymous"
           />
-                </div>
-        </Router>
-
-
+        </div>
+      </Router>
     );
   }
 }
-
-
 
 export default connect(null, { fetchFavorites })(App);
