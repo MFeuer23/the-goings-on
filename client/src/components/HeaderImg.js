ed0da9feca99215911792fc49d0e26320ab7966c
Mallory <mallory.feuer@gmail.com>
2019-03-18 03:17:04 -0400

    add Header component

---
+++ client/src/components/HeaderImg.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image'
+
+const Header = props => {
+
+
+  return(
+    <img src="/goings.jpg" style={{width: '50%' }}/>
+  )
+
+}
+
+export default Header;
