ff2fa7a521424ee73264f5ee6e45e8b372d668b5
Mallory <mallory.feuer@gmail.com>
2019-03-18 21:11:46 -0400

    remove console.logs

--- client/src/reducers/favorites_reducer.js
+++ client/src/reducers/favorites_reducer.js
@@ -5,19 +5,15 @@ function eventsReducer(state = {
 }, action) {
   switch (action.type) {
     case "CREATE_FAVORITE":
-      console.log(action)
       return {...state, favoritesData: [...state.favoritesData, action.payload]}
 
     case "LOADING_FAVORITES":
-      console.log(action)
       return {...state, loading: true, favoritesData:[], }
 
     case "FETCH_FAVORITES":
-      console.log(action)
       return {...state, loading:false, favoritesData: action.payload}
 
     case "DELETE_FAVORITE":
-      console.log(action)
       return {...state, loading: false,
       favoritesData: state.favoritesData.filter(
         favorite => favorite.id !== action.payload.id
