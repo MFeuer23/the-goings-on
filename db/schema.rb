396b1267773edd5b8bb9693a23800b562483f669
Mallory <mallory.feuer@gmail.com>
2019-03-18 21:11:04 -0400

    dropped unneeded events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_16_142142) do
+ActiveRecord::Schema.define(version: 2019_03_18_232657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "events", force: :cascade do |t|
-    t.string "title"
-    t.string "category"
-    t.boolean "ongoing"
-    t.string "article_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "favorites", force: :cascade do |t|
     t.string "title"
